cbaf12e8182543d4a1e5519aaf8339793e36d7ce
Guilherme Abel <guilhermeabel8@gmail.com>
2023-09-13 20:06:47 -0300

    feat: saving audio from audio calls

--- main.go
+++ main.go
@@ -6,11 +6,12 @@ import (
 	"os"
 	"os/signal"
 	"strings"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
-	// "github.com/pion/rtp"
-	// "github.com/pion/webrtc/v3/pkg/media"
-	// "github.com/pion/webrtc/v3/pkg/media/oggwriter"
+	"github.com/pion/rtp"
+	"github.com/pion/webrtc/v3/pkg/media"
+	"github.com/pion/webrtc/v3/pkg/media/oggwriter"
 )
 
 var (
@@ -34,12 +35,28 @@ func main() {
 		return
 	}
 
+	dg.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuildVoiceStates)
+
 	err = dg.Open()
 
 	if err != nil {
 		fmt.Println("error opening connection,", err)
 		return
 	}
+
+	v, err := dg.ChannelVoiceJoin(GuildID, ChannelID, true, false)
+	if err != nil {
+		fmt.Println("failed to join voice channel:", err)
+		return
+	}
+
+	go func() {
+		time.Sleep(10 * time.Second)
+		close(v.OpusRecv)
+		v.Close()
+	}()
+
+	handleVoice(v.OpusRecv)
 
 	dg.AddHandler(messageCreate)
 
@@ -62,6 +79,45 @@ func main() {
 	}()
 
 	select {}
+}
+
+func handleVoice(c chan *discordgo.Packet) {
+	files := make(map[uint32]media.Writer)
+	for p := range c {
+		file, ok := files[p.SSRC]
+		if !ok {
+			var err error
+			file, err = oggwriter.New(fmt.Sprintf("%d.ogg", p.SSRC), 48000, 2)
+			if err != nil {
+				fmt.Printf("failed to create file %d.ogg, giving up on recording: %v\n", p.SSRC, err)
+				return
+			}
+			files[p.SSRC] = file
+		}
+
+		rtp := createPionRTPPacket(p)
+		err := file.WriteRTP(rtp)
+		if err != nil {
+			fmt.Printf("failed to write to file %d.ogg, giving up on recording: %v\n", p.SSRC, err)
+		}
+	}
+
+	for _, f := range files {
+		f.Close()
+	}
+}
+
+func createPionRTPPacket(p *discordgo.Packet) *rtp.Packet {
+	return &rtp.Packet{
+		Header: rtp.Header{
+			Version:        2,
+			PayloadType:    0x78,
+			SequenceNumber: p.Sequence,
+			Timestamp:      p.Timestamp,
+			SSRC:           p.SSRC,
+		},
+		Payload: p.Opus,
+	}
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
