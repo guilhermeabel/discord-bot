b57257ddcfe3a51531062c4da92e1b0d57ee6aa2
Guilherme Abel <guilhermeabel8@gmail.com>
2023-09-14 20:41:32 -0300

    feat: using server in main func

--- main.go
+++ main.go
@@ -1,30 +1,18 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
-	"os/signal"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
-	// "github.com/pion/rtp"
-	// "github.com/pion/webrtc/v3/pkg/media"
-	// "github.com/pion/webrtc/v3/pkg/media/oggwriter"
 )
 
 var (
-	Token     string
-	ChannelID string
-	GuildID   string
+	Token     string = os.Getenv("TOKEN")
+	ChannelID string = os.Getenv("CHAT")
+	GuildID   string = os.Getenv("GUILD")
 )
-
-func init() {
-	flag.StringVar(&Token, "t", "", "Bot Token")
-	flag.StringVar(&GuildID, "g", "", "Guild in which voice channel exists")
-	flag.StringVar(&ChannelID, "c", "", "Voice channel to connect to")
-	flag.Parse()
-}
 
 func main() {
 	dg, err := discordgo.New("Bot " + Token)
@@ -46,22 +34,13 @@ func main() {
 	fmt.Println("bot is now running, press CTRL-C to exit.")
 
 	vc, err := connectToVoiceChannel(dg, GuildID, ChannelID)
+
+	app := &applicationServer{}
+	err = app.server().ListenAndServe()
+
 	if err != nil {
 		panic(err)
 	}
-
-	// on CTRL-C, close connection
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	go func() {
-		<-c
-		fmt.Println("closing connection")
-		vc.Disconnect()
-		dg.Close()
-		os.Exit(0)
-	}()
-
-	select {}
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
